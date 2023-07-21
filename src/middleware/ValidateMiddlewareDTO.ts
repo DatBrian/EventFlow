1d6ea855ba55b66926365d5e5dd7390d7186df1a
DatBrian <vikard.2005@hotmail.com>
2023-07-20 23:03:33 -0500

    refactor: :art: Se identó correctamente el archivo de middleware de validacion

--- src/middleware/ValidateMiddlewareDTO.ts
+++ src/middleware/ValidateMiddlewareDTO.ts
@@ -1,24 +1,24 @@
-    import { NextFunction, Request, Response } from "express";
-    import { plainToClass } from "class-transformer";
-    import { validate } from "class-validator";
+import { NextFunction, Request, Response } from "express";
+import { plainToClass } from "class-transformer";
+import { validate } from "class-validator";
 
-    class ValidateMiddlewareDTO {
-        constructor() { }
+class ValidateMiddlewareDTO {
+    constructor() { }
 
-        public static async validator(
-            req: Request,
-            res: Response,
-            next: NextFunction,
-            type: any
-        ) {
-            try {
-                const dto = plainToClass(type, req.body);
-                await validate(dto);
-                return ((req.body = dto), next());
-            } catch (error: any) {
-                console.error(error.message);
-                return res.status(500).json({ error: "Error en los parámetros de la consulta, revisa la consola para mas información" });
-            }
+    public static async validator(
+        req: Request,
+        res: Response,
+        next: NextFunction,
+        type: any
+    ) {
+        try {
+            const dto = plainToClass(type, req.body);
+            await validate(dto);
+            return ((req.body = dto), next());
+        } catch (error: any) {
+            console.error(error.message);
+            return res.status(500).json({ error: "Error en los parámetros de la consulta, revisa la consola para mas información" });
         }
     }
-    export default ValidateMiddlewareDTO;
+}
+export default ValidateMiddlewareDTO;

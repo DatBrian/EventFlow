21ca13faa4cda30de26d3d1dbcf3313e8bbd6369
DatBrian <vikard.2005@hotmail.com>
2023-07-20 21:58:19 -0500

    ci: :card_file_box: Se cambió el campo teléfono de la tabla usuario en la base de datos
    Se cambió el tamaño máximo de 15 a 20 en el campo teléfono

--- backup.sql
+++ backup.sql
@@ -195,7 +195,7 @@ CREATE TABLE `usuario` (
   `id` int NOT NULL AUTO_INCREMENT,
   `nombre` varchar(45) NOT NULL,
   `correo` varchar(76) NOT NULL,
-  `telefono` varchar(15) NOT NULL,
+  `telefono` varchar(20) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
